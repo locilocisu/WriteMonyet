d5afbc4915487807afc196a25138038d24602955
Aldo Sunaryo <clemensaldo@gmail.com>
2012-02-18 11:08:47 -0600

    Rename Kamus to KamusParser

---
+++ KamusParser/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System.Text;
+
+namespace Kamus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder output = new StringBuilder();
+
+            // Create an XmlReader
+            using (XmlReader reader = XmlReader.Create("C:\\Users\\locilocisu\\Dropbox\\Kamus Besar Bahasa Indonesia.xml"))
+            {
+                int i = 0;
+                while (i < 30000)
+                {
+                    reader.ReadToFollowing("text ");
+                    reader.MoveToAttribute("left");
+                    string leftPx = reader.Value;
+                    reader.MoveToAttribute("font");
+                    string fontSize = reader.Value;
+                    if (fontSize.Equals("13") && (leftPx.Equals("378") || leftPx.Equals("86") || leftPx.Equals("162")))
+                    {
+                        reader.MoveToElement();
+                        if (reader.ReadToDescendant("b"))
+                        {
+                            Console.WriteLine(reader.ReadElementContentAsString());
+                        }
+                        
+                    }
+
+                    i++;
+                }
+                
+            }
+
+           Console.ReadLine();
+        }
+    }
+}
